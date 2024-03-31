4f5656475384ae7761c7a753967c157c7e356f66
Amelievne <amelie.vne@gmail.com>
2024-03-31 16:59:28 +0200

    Setup the AI API with its function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+function displayIdea(response) {
+  console.log("generated");
+
+  new Typewriter("#ia-response", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 40,
+    cursor: "🎁",
+  });
+}
 function generateIdea(event) {
   event.preventDefault();
 
-  let iaResponse = document.querySelector("#ia-response");
-  new Typewriter(iaResponse, {
-    strings: "Here is an idea",
-    autoStart: true,
-    delay: 40,
-  });
+  let whoElement = document.querySelector("#who");
+
+  let aiApiKey = "974f7528a7fe8f1a738ftd6eob928053";
+  let aiApiPrompt = `User instruction : Generate gift ideas for ${whoElement.value}`;
+  let aiApiContext =
+    "You are a personal shopper and your mission is to give one and only one personalized gift idea. Make sure to use the user instruction and to provide a short answer.";
+  let aiApiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${aiApiPrompt}&context=${aiApiContext}&key=${aiApiKey}`;
+
+  console.log(aiApiPrompt);
+  axios.get(aiApiUrl).then(displayIdea);
 }
 
 let inputsFormsElement = document.querySelector("#ia-inputs");
