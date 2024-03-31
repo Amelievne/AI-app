f8288724c744f27d7d4273bc7a1d944783576b98
Amelievne <amelie.vne@gmail.com>
2024-03-31 16:23:19 +0200

    Added JS function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+function generateIdea(event) {
+  event.preventDefault();
+  alert("Hello");
+  let iaResponse = document.querySelector("#ia-response");
+  iaResponse.innerHTML = "Here is an idea";
+}
 
+let inputsFormsElement = document.querySelector("#ia-inputs");
+inputsFormsElement.addEventListener("submit", generateIdea);
