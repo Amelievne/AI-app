4b24b10fa757435910f35138469a9876decf56ca
Amelievne <amelie.vne@gmail.com>
2024-03-31 19:10:41 +0200

    Actualized IA context and prompt

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ function generateIdea(event) {
   The person's hobbies are: ${hobbiesElement.value}. 
   It must be under the price of ${budgetElement.value}`;
   let aiApiContext =
-    "You are a personal shopper and your mission is to give one and only one gift idea. Make sure to use the user instruction bellow to personalized the answer and to provide a short answer with only the anwser for exemple: a customized wallet from Parfois that will cost about 25euros.";
+    "You are a personal shopper and your mission is to give one and only one gift idea. You must provide a short answer with only the anwser for exemple: a customized wallet from Longchamp that will cost about 65euros. Make sure to use the user instruction bellow to personalized the answer. The price must be occurate and based on real prices";
   let aiApiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${aiApiPrompt}&context=${aiApiContext}&key=${aiApiKey}`;
 
   let responseSection = document.querySelector("#response-section");
